30f9936145b80145b34f7df14b7124b0f43e3b56
Forest <fhaughton6@gmail.com>
2024-10-17 08:28:49 -0400

    1

--- src/TempConversion.java
+++ src/TempConversion.java
@@ -1,5 +1,51 @@
-public class Main {
+import java.util.Scanner;
+
+public class TempConversion {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        //Variables
+        double userTemp = 0;
+        double fahrenheit;
+        boolean valid = false;
+        boolean rematch = false;
+        String userRematch;
+
+        while (!rematch) {
+            //inputs
+            System.out.println("Please in put a valid temperature in celsius");
+            do {
+                if (scan.hasNextDouble()) {
+                    userTemp = scan.nextDouble();
+                    valid = true;
+
+                }
+                else {
+                    System.out.println("You have given an invalid temperature. Please try again");
+                    scan.nextLine();
+                }
+            } while (!valid);
+
+            fahrenheit = ((double) 9 /5) * userTemp + 32;
+
+            //output
+            System.out.printf("%5s %10.2f %5s %10.2f", "Degrees C: ", userTemp, "\nDegrees F: ", fahrenheit);
+
+            System.out.println("\nDo you want to input another temperature? [Y/N]");
+            scan.nextLine();
+            userRematch = scan.nextLine();
+            if (userRematch.equalsIgnoreCase("Y")) {
+                rematch = false;
+            }
+            else if (userRematch.equalsIgnoreCase("N")) {
+                rematch = true;
+            }
+            else {
+                System.out.println("You inputted an incorrect answer, defaulting to N");
+                rematch = true;
+            }
+
+
+        }
     }
 }
